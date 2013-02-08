cecd0d01eb5dfe86b724e2ea0a732e98616f6338
Jojo <joseph.rahamim@tufts.edu>
2013-02-08 17:03:41 -0500

    Game loop added, parameter initialization method created but useless

--- frogger/game.js
+++ frogger/game.js
@@ -1,1 +1,34 @@
-// Your work goes here...
+function start_game()
+{
+	gameLoop(initializeParameters());
+}
+
+//Initializes game parameters
+function initializeParameters()
+{
+	var frogx = 0;
+	var frogy = 0;
+	var lives = 3;
+	var gameover = false;
+	var level_number = 1;
+	var time = 0;
+	var vehicle_spawn_locations = [[0,0],[0,0],[0,0],[0,0],[0,0]];
+	var log_spawn_locations = [[0,0],[0,0],[0,0],[0,0],[0,0]];
+	var vehicle_speed = 0;
+	var log_speed = 0;
+	return frogx, frogy
+}
+
+//This function handles the game loop
+function gameLoop(frogx, frogy, lives, gameover, level_number, time, vehicle_spawn_locations,
+					log_spawn_locations, vehicle_speed, log_speed)
+{
+	console.log(frogy);
+	var gameon = false; //for now
+	
+	while(gameon)
+	{
+		
+	}
+	
+}
