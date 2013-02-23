82d30728781966b5f9c0c243c5d83b0a52c12618
Jojo <joseph.rahamim@tufts.edu>
2013-02-23 15:58:49 -0500

    map renders

--- where/T_mapper.js
+++ where/T_mapper.js
@@ -1,3 +1,8 @@
 function run(){
-
+	 var myOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 }
