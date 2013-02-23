cb1505931a5c2533eda9ffdfd241c5e72344b442
Jojo <joseph.rahamim@tufts.edu>
2013-02-23 16:46:08 -0500

    Hard data coding started

--- where/T_mapper.js
+++ where/T_mapper.js
@@ -1,8 +1,30 @@
 function run(){
-	 var myOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
+	var myOptions = {
+          center: new google.maps.LatLng(44.0603 , -69.3583),
           zoom: 8,
           mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+         };
 	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	
+	load_hard_data();
+	
+	var marker = new google.maps.Marker({
+      position: station_list[0].position,
+      map: map,
+      title:station_list[0].name,
+    });
+}
+
+//Station Class
+function station(name, id, lat, lon)
+{
+	this.name = name;
+	this.id = id;
+	this.position = new google.maps.LatLng(lat, lon, false);
+}
+
+function load_hard_data()
+{
+	Alewife = new station("Alewife", "place-alfcl", 42.395428, -71.142483);
+	station_list = [Alewife];
 }
