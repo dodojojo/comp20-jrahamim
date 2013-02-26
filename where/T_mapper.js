ee2b50de43d883ce5e949918d64c4e77f9d924f5
Jojo <joseph.rahamim@tufts.edu>
2013-02-26 13:09:57 -0500

    Hard dat added

--- where/T_mapper.js
+++ where/T_mapper.js
@@ -6,18 +6,9 @@ function run(){
          };
 	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 	
-	load_hard_data();
-	//create_markers();
-	for(var i = 0; i < station_list.length; i++)
-	{
-			google.maps.event.addListener(station_list[i].marker, 'click', function(){
-   		 	this.parent.display_info();
-    		});
-	}
-	
+	create_stations();
+	create_marker_listeners();
 	draw_trainline();
-	
-	
 }
 
 //Station Class
@@ -44,36 +35,48 @@ function station(name, north_key, south_key, lat, lon)
     }
 }
 
-function load_hard_data()
+function create_stations()
 {
 	Alewife = new station("Alewife", "RALEN", "none", 42.395428, -71.142483);
 	Davis = new station("Davis", "RDAVN", "RDAVS", 42.39674, -71.121815 );
 	Porter = new station("Porter", "RPORN", "RPORS", 42.3884, -71.119149);
 	Harvard = new station("Harvard", "RHARN", "RHARS", 42.373362, -71.118956);
-	Central = new station();
-	Kendall = new station();
-	Charles_MGH = new station();
-	Park = new station();
-	Downtown_crossing = new station();
-	South_station = new station();
-	Broadway = new station();
-	Andrew = new station();
-	JFK = new station();
-	Savin_hill = new station();
-	Fields_corner = new station();
-	Shawmut = new station();
-	Ashmont = new station();
-	North_quincy = new station();
-	Wollaston = new station();
-	Quincy_center = new station();
-	Quincy_adams = new station();
-	Braintree = new station();
+	Central = new station("Central", "RCENN", "RCENS", 42.365486, -71.103802);
+	Kendall = new station("Kendall", "RKENN", "RKENS", 42.36249079, -71.08617653);
+	Charles_MGH = new station("Charles MGH", "RMGHN", "RMGHS", 42.361166, -71.070628);
+	Park = new station("Park", "RPRKN", "RPRKS", 42.35639457, -71.0624242);
+	Downtown_crossing = new station("Downtown Crossing", "RDTCN", "RDTCS", 42.355518, -71.060225);
+	South_station = new station("South Station", "RSOUN", "RSOUN", 42.352271, -71.055242);
+	Broadway = new station("Broadway", "RBRON", "RBROS", 42.342622, -71.056967);
+	Andrew = new station("Andrew", "RANDN", "RANDS", 42.330154, -71.057655);
+	JFK = new station("JFK", "RJFKN", "RJFKS", 42.320685, -71.052391);
+	Savin_hill = new station("Savin Hill", "RSAVN", "RSAVS", 42.31129, -71.053331);
+	Fields_corner = new station("Fields Corner", "RFIEN", "RFIES", 42.300093, -71.061667);
+	Shawmut = new station("Shawmut", "RSHAN", "RSHAS", 42.29312583, -71.06573796);
+	Ashmont = new station("Ashmont", "none", "RASHS", 42.284652, -71.064489);
+	North_quincy = new station("North Quincy", "RNQUN", "RNQUS", 42.275275, -71.029583);
+	Wollaston = new station("Wollaston", "RWOLN", "RWOLS", 42.2665139, -71.0203369);
+	Quincy_center = new station("Quincy Center", "RQUCN", "RQUCS", 42.251809, -71.005409);
+	Quincy_adams = new station("Quincy Adams", "RQUAN", "RQUAS", 42.233391, -71.007153);
+	Braintree = new station("Braintree", "none", "RBRAS", 42.2078543, -71.0011385);
 	
 	station_list = [Alewife, Davis, Porter, Harvard, Central, Kendall, Charles_MGH, Park,
 					 Downtown_crossing, South_station, Broadway, Andrew, JFK, Savin_hill,
 					 Fields_corner, Shawmut, Ashmont, North_quincy, Wollaston, 
 					 Quincy_center, Quincy_adams, Braintree];
 }
+
+function create_marker_listeners()
+{
+	for(var i = 0; i < station_list.length; i++)
+	{
+			google.maps.event.addListener(station_list[i].marker, 'click', function(){
+   		 	this.parent.display_info();
+    		});
+	}
+	
+}
+
 
 function draw_trainline()
 {
